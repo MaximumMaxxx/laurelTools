5d5e5a2fdd4e72135e968b84cc3455c133cd08a2
Max <maxwellwindland@gmail.com>
2023-02-01 23:41:53 -0800

    Switched to flask sqlalchemy

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
+import time
+
 from yt_dlp import YoutubeDL
 from flask import Flask, render_template, request, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
 import logging
 import uuid
-from collections import namedtuple
+from dataclasses import dataclass
 
+db = SQLAlchemy()
 app = Flask(__name__)
 app.config.update(
     DOWNLOAD_DIR="downloads",
+    SQLALCHEMY_DATABASE_URI="sqlite:///project.db"
 )
+db.init_app(app)
+
+
+class Download(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String)
+    uuid = db.Column(db.String, unique=True)
+    remove = db.Column(db.Integer)
+    url = db.Column(db.String)
+
+
+with app.app_context():
+    db.create_all()
 
 logging.basicConfig(level=logging.DEBUG)
 
-# A little global config
-Tvideo = namedtuple("download", "format videoUrl extension")
-AUDIO = "A"
-AUDIO_VIDEO = "B"
+AUDIO = "wav"
+AUDIO_VIDEO = "mp4"
+
 
 # Simple state management for downloads
-downloads: "dict[str, video]" = {}
-
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -31,28 +46,77 @@ def video():
     # Get video info
     id = uuid.uuid4().hex
     try:
-        with YoutubeDL() as downloader:
-            info = downloader.extract_info(
+        with YoutubeDL() as infoExtractor:
+            info = infoExtractor.extract_info(
                 url, download=False
             )
         videoInfo = {"thumbnail": info['thumbnail'], "dlid": id}
     except Exception:
         videoInfo = None
-    vid = Tvideo
-    vid.videoUrl = url
-    downloads[id] = vid
+    print("Made it past downloading!")
+
+    db.session.execute(db.insert(Download))
+
+    vidDownload = Download(
+        url=url,
+        type=None,
+        uuid=id,
+        remove=int(time.time()) + 604800  # 7 days
+    )
+    db.session.add(vidDownload)
+    db.session.commit()
     return render_template("index.html", videoInfo=videoInfo, download=None)
 
 
 @app.post('/waiting/<dlid>')
 def waiting(dlid):
-    downloads[dlid].type = request.form.get("Type")
+    dltype = request.form.get("Type")
+
+    db.session.execute(
+        db.update(Download)
+        .where(Download.uuid == dlid)
+        .values(
+            type=dltype
+        )
+    )
+    db.session.commit()
+
     return render_template("loading.html", dlid=dlid)
 
 
 @app.route("/download/<dlid>")
 def download(dlid):
-    dlidVideo = downloads[dlid]
+    dlidVideo = db.session.execute(db.select(Download).where(Download.uuid == dlid)).scalar()
+
+    otherDownload = db.session.execute(
+        db.select(Download)
+        .where(Download.url == dlidVideo.url)
+        .where(Download.type == dlidVideo.type)
+    ).scalars().all()
+
+    if len(otherDownload) >= 2:
+        # We've already downloaded the file
+        # Saves time and money for me and the end user
+
+        # Select the duplicate we just made, so we can remove it
+        dl = db.session.execute(
+            db.select(Download)
+            .where(Download.uuid == dlid)
+        ).scalar()
+
+        db.session.delete(dl)
+        db.session.commit()
+
+        ogdl = db.session.execute(
+            db.select(Download)
+            .where(Download.uuid != dlid)
+            .where(Download.type == dlidVideo.type)
+            .where(Download.url == dlidVideo.url)
+        ).scalar()
+
+        return render_template("index.html", download=f"{ogdl.uuid}.{ogdl.type}", videoInfo=None)
+
+
     dllocation = f"./{app.config['DOWNLOAD_DIR']}/{dlid}"
     opts = {}
 
@@ -65,7 +129,6 @@ def download(dlid):
                 'preferredquality': '192',
             }],
         }
-        dlidVideo.extension = ".wav"
     else:
         opts = {
             "outtmpl": dllocation,
@@ -74,12 +137,10 @@ def download(dlid):
                 'preferedformat': 'mp4',
             }],
         }
-        dlidVideo.extension = ".mp4"
 
     with YoutubeDL(opts) as downloader:
-        downloader.download([dlidVideo.videoUrl])
-    downloads.pop(dlid)
-    return render_template("index.html", download=dlid + dlidVideo.extension, videoInfo=None)
+        downloader.download([dlidVideo.url])
+    return render_template("index.html", download=f"{dlid}.{dlidVideo.type}", videoInfo=None)
 
 
 @app.get(f"/{app.config['DOWNLOAD_DIR']}/<path:path>")
